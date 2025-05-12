8a98895a6370bc4c3c7f07380b911659d63e1c3e
Halcyon Skinner <hdskinner@gmail.com>
2025-05-12 18:46:04 -0400

    Init commit

--- malloc/my_malloc.c
+++ malloc/my_malloc.c
@@ -4,6 +4,33 @@
 #include <string.h>
 #include <sys/mman.h>
 
+
+struct heap_data {
+    struct chunk *start; 
+    uint32_t avail_mem;
+}
+
+struct chunk {
+    uint32_t size;
+    bool in_use;
+    struct chunk *next;
+}
+
+
+
 int main(int argc, char *argv[]) {
-    void *start = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
+    void *entry = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
+
+    struct heap_data heap; 
+    heap.start = entry;
+    heap.size = getpagesize();
+}
+
+
+void *heap_alloc(uint32_t size) {
+
+}
+
+void heap_free(void *ptr) {
+    
 }
