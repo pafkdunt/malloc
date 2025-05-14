a0fdecb9ba11d6d96ae91b5647d3d312e9a8fd5e
pafkdunt <sharma.rohan4@northeastern.edu>
2025-05-13 21:53:17 -0400

    Created heap and chunk data structures to have a format to our heap

--- malloc/my_malloc.c
+++ malloc/my_malloc.c
@@ -3,34 +3,48 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <stdbool.h>
+
 
 
 struct heap_data {
     struct chunk *start; 
     uint32_t avail_mem;
-}
+};
 
 struct chunk {
     uint32_t size;
     bool in_use;
     struct chunk *next;
-}
+};
 
 
 
 int main(int argc, char *argv[]) {
-    void *entry = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
+    void *entry = mmap(NULL, 320, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+
 
     struct heap_data heap; 
     heap.start = entry;
-    heap.size = getpagesize();
+    heap.avail_mem = 320;
+
+    printf("%p\n", entry);
+
+    struct chunk *c1;
+    c1->size = 32;
+    c1->in_use = true;
+    c1->next = NULL;
+
+    c1 = ((struct chunk *) entry) + 1;
+
+    printf("%p\n", c1);
 }
 
 
-void *heap_alloc(uint32_t size) {
+// void *heap_alloc(uint32_t size) {
 
-}
+// }
 
-void heap_free(void *ptr) {
+// void heap_free(void *ptr) {
     
-}
+// }
